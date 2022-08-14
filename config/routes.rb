709e855a8159f3c1733b7f14bac15e8d50f5cdae
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-08-14 08:42:37 +0000

    【Update】１６章完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   
   get '/post_images' => 'post_images#index', as: 'post_images'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  
+  get 'users/:id/edit' => 'users#edit', as: 'edit_users'
+  resources :users, only: [:show, :edit, :update]
   
   get '/homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
